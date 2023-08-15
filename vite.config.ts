6c133a20f732892b19bf11b2d4f8d11b267b00d4
Adrian Burlacu <adrian.burlacu@live.com>
2023-08-15 13:46:25 +0300

    Links!

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/being-locator/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
